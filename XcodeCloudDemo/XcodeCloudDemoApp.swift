1618997dcd41d9c605a37e7188a99de05f148fe2
Mohammad Azam <azamsharp@gmail.com>
2024-01-29 14:07:01 -0600

    starter project

---
+++ XcodeCloudDemo/XcodeCloudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudDemoApp.swift
+//  XcodeCloudDemo
+//
+//  Created by Mohammad Azam on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
